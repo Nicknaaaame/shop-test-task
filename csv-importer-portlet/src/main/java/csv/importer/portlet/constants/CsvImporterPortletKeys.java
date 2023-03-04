75cb246b71bf00c112fc47750d9a070dafc9f5c3
levin_main <dlyaregistr123@bk.ru>
2023-03-04 16:28:47 +0700

    create empty module

---
+++ csv-importer-portlet/src/main/java/csv/importer/portlet/constants/CsvImporterPortletKeys.java
@@ -0,0 +1,11 @@
+package csv.importer.portlet.constants;
+
+/**
+ * @author dlyar
+ */
+public class CsvImporterPortletKeys {
+
+	public static final String CSVIMPORTER =
+		"csv_importer_portlet_CsvImporterPortlet";
+
+}
