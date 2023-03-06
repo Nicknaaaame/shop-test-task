374e8277f1a22404fe8a4f1784b539bc611af79d
levin_main <dlyaregistr123@bk.ru>
2023-03-06 19:09:36 +0700

    add electronics fk check

--- electro-portlet/src/main/java/electro/portlet/portlet/ElectronicsPortlet.java
+++ electro-portlet/src/main/java/electro/portlet/portlet/ElectronicsPortlet.java
@@ -8,7 +8,9 @@ import com.liferay.portal.kernel.util.ParamUtil;
 import electro.portlet.constants.ElectroPortletKeys;
 import electro.portlet.mapper.ElectronicsModelMapper;
 import org.osgi.service.component.annotations.Component;
+import shop.exception.NoSuchElectroTypeException;
 import shop.model.Electronics;
+import shop.service.ElectroTypeLocalServiceUtil;
 import shop.service.ElectronicsLocalServiceUtil;
 import shop.service.EmployeeLocalServiceUtil;
 import util.ShopProjectUtil;
@@ -21,45 +23,56 @@ import javax.portlet.Portlet;
  * @author dlyar
  */
 @Component(
-	immediate = true,
-	property = {
-		"com.liferay.portlet.display-category=category.sample",
-		"com.liferay.portlet.header-portlet-css=/css/main.css",
-		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=Electro",
-		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.name=" + ElectroPortletKeys.ELECTRONICS,
-		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
-	},
-	service = Portlet.class
+        immediate = true,
+        property = {
+                "com.liferay.portlet.display-category=category.sample",
+                "com.liferay.portlet.header-portlet-css=/css/main.css",
+                "com.liferay.portlet.instanceable=true",
+                "javax.portlet.display-name=Electro",
+                "javax.portlet.init-param.template-path=/",
+                "javax.portlet.init-param.view-template=/view.jsp",
+                "javax.portlet.name=" + ElectroPortletKeys.ELECTRONICS,
+                "javax.portlet.resource-bundle=content.Language",
+                "javax.portlet.security-role-ref=power-user,user"
+        },
+        service = Portlet.class
 )
 public class ElectronicsPortlet extends MVCPortlet {
-	public void addElectronics(ActionRequest request, ActionResponse response) throws SystemException {
-		long id = CounterLocalServiceUtil.increment();
-		Electronics electronics = ElectronicsLocalServiceUtil.createElectronics(id);
-		ElectronicsModelMapper.map(request, electronics);
-		ElectronicsLocalServiceUtil.addElectronics(electronics);
-	}
+    public void addElectronics(ActionRequest request, ActionResponse response) throws SystemException {
+        long id = CounterLocalServiceUtil.increment();
+        try {
+            Electronics electronics = ElectronicsLocalServiceUtil.createElectronics(id);
+            ElectronicsModelMapper.map(request, electronics);
+            checkForeignKeys(electronics);
+            ElectronicsLocalServiceUtil.addElectronics(electronics);
+        } catch (NoSuchElectroTypeException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public void updateElectronics(ActionRequest request, ActionResponse response) throws SystemException {
-		long id = ParamUtil.getLong(request, "id");
-		try {
-			Electronics electronics = ElectronicsLocalServiceUtil.getElectronics(id);
-			ElectronicsModelMapper.map(request, electronics);
-			ElectronicsLocalServiceUtil.updateElectronics(electronics);
-		} catch (PortalException e) {
-			ShopProjectUtil.handleException(request, response, e);
-		}
-	}
+    public void updateElectronics(ActionRequest request, ActionResponse response) throws SystemException {
+        long id = ParamUtil.getLong(request, "id");
+        try {
+            Electronics electronics = ElectronicsLocalServiceUtil.getElectronics(id);
+            ElectronicsModelMapper.map(request, electronics);
+            checkForeignKeys(electronics);
+            ElectronicsLocalServiceUtil.updateElectronics(electronics);
+        } catch (PortalException e) {
+            ShopProjectUtil.handleException(request, response, e);
+        }
+    }
 
-	public void deleteElectronics(ActionRequest request, ActionResponse response) {
-		long id = ParamUtil.getLong(request, "id");
-		try {
-			EmployeeLocalServiceUtil.deleteEmployee(id);
-		} catch (PortalException e) {
-			ShopProjectUtil.handleException(request, response, e);
-		}
-	}
+    public void deleteElectronics(ActionRequest request, ActionResponse response) {
+        long id = ParamUtil.getLong(request, "id");
+        try {
+            EmployeeLocalServiceUtil.deleteEmployee(id);
+        } catch (PortalException e) {
+            ShopProjectUtil.handleException(request, response, e);
+        }
+    }
+
+    private void checkForeignKeys(Electronics product) throws NoSuchElectroTypeException {
+        if (ElectroTypeLocalServiceUtil.fetchElectroType(product.getETypeId()) == null)
+            throw new NoSuchElectroTypeException(product.getETypeId());
+    }
 }
