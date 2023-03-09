df41dff8a5cd47443a80e19dccfb5b3816688f30
levin_main <dlyaregistr123@bk.ru>
2023-03-09 12:35:47 +0700

    add empty card module

---
+++ card/src/main/java/card/constants/CardPortletKeys.java
@@ -0,0 +1,11 @@
+package card.constants;
+
+/**
+ * @author dlyar
+ */
+public class CardPortletKeys {
+
+	public static final String CARD =
+		"card_CardPortlet";
+
+}
---
+++ card/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="card.caption"/></b>
+</p>
